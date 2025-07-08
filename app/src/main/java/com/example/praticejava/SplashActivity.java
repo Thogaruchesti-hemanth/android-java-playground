18ed13c7400ea089510d0a6f57753c31e212e933
Thogaruchsesti-Hemanth <saihemanth225@g,ail.com>
2025-07-09 00:16:30 +0530

    Added splash screen and dark mode support

--- app/src/main/java/com/example/praticejava/SplashActivity.java
+++ app/src/main/java/com/example/praticejava/SplashActivity.java
@@ -1,41 +1,36 @@
 package com.example.praticejava;
 
+import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TASK;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
-import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    private boolean isAppReady = false;
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        splashScreen.setKeepOnScreenCondition(() -> !isAppReady);
 
-        initializeApp();
-    }
-
-    private void initializeApp() {
-
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            //all the initialize done here and then
-            isAppReady = true;
-        }, 200);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(this,MainActivity.class);
+            intent.addFlags(FLAG_ACTIVITY_NEW_TASK |FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        },2000);
     }
 }
